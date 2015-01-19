bcad2f0cb4ed88b1c26f79d3cd0b48b6f2980968
Tomasz Pluskiewicz <tomasz@t-code.pl>
2015-01-19 15:23:09 +0100

    extract global app initialization module

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+define(['HistoryStore', 'ModelStore'], function() {   
+  'use strict';
+
+  function getQueryVariable(variable)
+  {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+      var pair = vars[i].split("=");
+      if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+  }
+
+  window.addEventListener('polymer-ready', function() {
+    var url = getQueryVariable('uri');
+    if (url == false) {
+      url = 'http://test.wikibus.org/';
+    }
+
+    var browser = document.querySelector('ld-browser');
+    browser.uri = url;
+    browser.load();
+  });
+});

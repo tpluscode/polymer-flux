f8e5d71cd278634b2c7a916ad18e8dd92065f7a0
Tomasz Pluskiewicz <tomasz@t-code.pl>
2015-01-20 12:00:56 +0100

    use xhr directly

--- src/js/modelStore.js
+++ src/js/modelStore.js
@@ -1,12 +1,34 @@
 (function() {
 
-  define(['reflux', 'jsonld', 'actions', 'qwest'], function(Reflux, jsonld, actions, qwest) {
+  define(['reflux', 'jsonld', 'actions', 'es6-promise'], function(Reflux, jsonld, actions, promise) {
     'use strict';
 
     var NavActions = actions.NavActions;
     var jsonld = jsonld.promises;
 
     var currentModel = { };
+
+    var executeXhr = function(uri) {
+      return new promise.Promise(function(resolve, reject) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open("GET", uri, true);
+        httpRequest.setRequestHeader('Accept','application/ld+json');
+        httpRequest.onreadystatechange = function()
+      {
+        if (httpRequest.readyState != 4) {
+          return;
+        }
+
+        if (httpRequest.status != 200) {
+          reject(httpRequest);
+        } else {
+          resolve(httpRequest.responseText);
+        }
+      };
+
+      httpRequest.send(null);
+    });
+  }
 
     return Reflux.createStore({
       init: function() {
@@ -17,17 +39,12 @@
 
         NavActions.beforeLoad(currentModel);
 
-        qwest.get(uri, null, {
-          headers: {
-            'Accept': 'application/ld+json'
-          }
-        }).then(function(res) {
+        executeXhr(uri).then(function(res) {
           return jsonld.expand(JSON.parse(res)).then(function(expanded) {
             NavActions.navigateTo.success(expanded[0]);
           });
-        }).catch(function (error) {
-          NavActions.navigateTo.failed(this.status);
-          throw error;
+        }, function (request) {
+          NavActions.navigateTo.failed(request.status);
         });
       }
     });

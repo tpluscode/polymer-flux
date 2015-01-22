b5b8a8fbb50575d240ee3736b52d9985a84024bd
Tomasz Pluskiewicz <tomasz@t-code.pl>
2015-01-22 14:07:41 +0100

    hide requirejs config

---
+++ dist/js/require.config.js
@@ -0,0 +1,11 @@
+requirejs.config({
+  baseUrl: '/dist/js/',
+  paths: {
+    jsonld: '/dist/lib/jsonld/js/jsonld',
+    'es6-promise': '/dist/lib/es6-promise/promise',
+    reflux: '/dist/lib/reflux/dist/reflux',
+    underscore: '/dist/lib/underscore/underscore',
+    react: '/dist/lib/react/react',
+    jquery: '/dist/lib/jquery/dist/jquery'
+  }
+});

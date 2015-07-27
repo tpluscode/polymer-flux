30fa8ac1050c0394e06928088796ae543b255ca6
tpluscode <tpluscode@users.noreply.github.com>
2015-07-27 22:40:16 +0200

    basic structure and routing

--- src/js/require.config.js
+++ src/js/require.config.js
@@ -1,12 +1,12 @@
 var require = {
-  baseUrl: '/js/',
-  paths: {
-    jsonld: '/lib/jsonld/js/jsonld',
-    'es6-promise': '/lib/es6-promise/promise',
-    reflux: '/lib/reflux/dist/reflux',
-    underscore: '/lib/underscore/underscore',
-    react: '/lib/react/react',
-    jquery: '/lib/jquery/dist/jquery',
-    URIjs: '/lib/uri.js/src'
-  }
+    baseUrl: '/js/',
+    paths: {
+        jsonld: '/lib/jsonld/js/jsonld',
+        'es6-promise': '/lib/es6-promise/promise',
+        reflux: '/lib/reflux/dist/reflux',
+        underscore: '/lib/underscore/underscore',
+        react: '/lib/react/react',
+        jquery: '/lib/jquery/dist/jquery',
+        URIjs: '/lib/uri.js/src'
+    }
 };
